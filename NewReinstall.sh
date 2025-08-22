d2a2d07e52ec6c16fe55824c995becc02d00e58f
virkingz <68280845+virkingz@users.noreply.github.com>
2025-08-22 08:23:29 +0800

    Update NewReinstall.sh

--- NewReinstall.sh
+++ NewReinstall.sh
@@ -154,9 +154,9 @@ function Start() {
   fi
 
   if [[ "$isCN" == '1' ]]; then
-   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://cdn.jsdelivr.net/gh/fcurrk/reinstall@master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
+   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://cdn.jsdelivr.net/gh/virkingz/reinstall@master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
   else 
-   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://raw.githubusercontent.com/fcurrk/reinstall/master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
+   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://raw.githubusercontent.com/virkingz/reinstall/master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
   fi
   
   CMIRROR=''
